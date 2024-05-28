005a1467a943bde9a2418214fc11e977110508a4
goliathgrouper <vitalijsimon48@gmail.com>
2024-05-28 18:06:38 +0300

    Add editing of cell state (dead or alive)

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const SIZE = 700;
 
-const container = document.querySelector('.container');
+const container = document.querySelector('#container');
 
 function getSquareSize(squareNumber) {
     return `${(SIZE/squareNumber)-2}px`;
@@ -13,10 +13,45 @@ function generateGrid(n) {
         square.style.border = '1px solid black';
         square.style.width = getSquareSize(n);
         square.style.height = getSquareSize(n);
-        square.style.backgroundColor = 'white';
+        square.classList.add('dead');
 
         container.appendChild(square);
     }
 }
 
 generateGrid(16);
+
+document.querySelector('#grid-size').addEventListener('click', (event) => {
+    let newGridSize;
+    do {
+        newGridSize = prompt('Select new grid-size (5 - 100)');
+    } while (newGridSize > 100 || newGridSize < 5);
+
+    const oldGrid = document.querySelectorAll('#container > div');
+    oldGrid.forEach(el => container.removeChild(el));
+    generateGrid(newGridSize);
+});
+
+function editGrid(event) {
+    if (event.target.className === 'dead') {
+        event.target.classList.remove('dead');
+        event.target.classList.add('alive');
+    } else {
+        event.target.classList.remove('alive');
+        event.target.classList.add('dead');
+    }
+}
+
+const editBtn = document.querySelector('#edit');
+let editState = false;
+editBtn.addEventListener('click', (event) => {
+    if (editState) {
+        editState = false;
+        container.removeEventListener('click', editGrid);
+    } else {
+        editState = true;
+        container.addEventListener('click', editGrid);
+    }
+
+});
+
