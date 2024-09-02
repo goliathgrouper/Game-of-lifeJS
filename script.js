af8adb4e8ad472dca749d1c2438f3926e3526eb3
goliathgrouper <vitalijsimon48@gmail.com>
2024-09-02 09:40:09 +0300

    Grid is a two-dim array, add neighbor count

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const SIZE = 700;
 
+const editBtn = document.querySelector('#edit');
+const strendbtn = document.querySelector('#start-end');
+
 const container = document.querySelector('#container');
 
+let gridSize;
 let grid = [];
 
 function getSquareSize(squareNumber) {
@@ -10,17 +14,23 @@ function getSquareSize(squareNumber) {
 
 function generateGrid(n) {
     let square;
-    
-    for (let i = 0; i < n**2; i++) {
-        square = document.createElement('div');
-        square.style.border = '1px solid black';
-        square.style.width = getSquareSize(n);
-        square.style.height = getSquareSize(n);
-        square.classList.add('dead');
-
-        container.appendChild(square);
+    gridSize = n;
+    let column;
+    for (let x = 0; x < n; x++) {
+        column = [];
+        for (let y = 0; y < n; y++) {
+            square = document.createElement('div');
+            square.style.border = '1px solid black';
+            square.style.width = getSquareSize(n);
+            square.style.height = getSquareSize(n);
+            square.classList.add('dead');
+            column.push(square);
+            container.appendChild(square);
+        }
+        grid.push(column);
+        
     }
-    grid = [...container.querySelectorAll('#container > div')];
+    //grid = [...container.querySelectorAll('#container > div')];
 }
 
 generateGrid(16);
@@ -49,9 +59,8 @@ function editGrid(event) {
     }
 }
 
-
 // Edit alive or dead by color
-const editBtn = document.querySelector('#edit');
+
 let editState = false;
 editBtn.addEventListener('click', (event) => {
     if (editState) {
@@ -68,12 +77,92 @@ editBtn.addEventListener('click', (event) => {
 
 });
 
-const strendbtn = document.querySelector('#start-end');
+
 strendbtn.addEventListener('click', (event) => {
     let text = strendbtn.textContent;
     if (text === 'Start') {
         strendbtn.textContent = 'End';
+        
     } else {
         strendbtn.textContent = 'Start';
+        
     }
 });
+
+
+for (let x = 0; x < gridSize; x++) {
+    for (let y = 0; y < gridSize; y++) {
+        leftCoord = (x - 1) % gridSize;
+        rightCoord = (x + 1) % gridSize;
+        aboveCoord = (y - 1) % gridSize;
+        belowCoord = (y + 1) % gridSize;
+
+        numNeighbors = 0
+        if (grid.at(leftCoord).at(aboveCoord).classList.contains('alive')) {
+            numNeighbors += 1;
+        }
+        if (grid.at(x).at(aboveCoord).classList.contains('alive')) {
+            numNeighbors += 1;
+        } 
+        if (grid.at(rightCoord).at(aboveCoord).classList.contains('alive')) {
+            numNeighbors += 1;
+        } 
+        if (grid.at(leftCoord).at(y).classList.contains('alive')) {
+            numNeighbors += 1;
+        } 
+        if (grid.at(rightCoord).at(y).classList.contains('alive')) {
+            numNeighbors += 1;
+        } 
+        if (grid.at(leftCoord).at(belowCoord).classList.contains('alive')) {
+            numNeighbors += 1;
+        } 
+        if (grid.at(x).at(belowCoord).classList.contains('alive')) {
+            numNeighbors += 1;
+        } 
+        if (grid.at(rightCoord).at(belowCoord).classList.contains('alive')) {
+            numNeighbors += 1;
+        } 
+    }
+}
+/*# Calculate the next step's cells based on current step's cells:
+    for x in range(gridSize):
+        for y in range(HEIGHT):
+            # Get neighboring coordinates:
+            # `% WIDTH` ensures leftCoord is always between 0 and WIDTH - 1
+            leftCoord  = (x - 1) % WIDTH
+            rightCoord = (x + 1) % WIDTH
+            aboveCoord = (y - 1) % HEIGHT
+            belowCoord = (y + 1) % HEIGHT
+
+            # Count number of living neighbors:
+            numNeighbors = 0
+            if currentCells[leftCoord][aboveCoord] == '#':
+                numNeighbors += 1 # Top-left neighbor is alive.
+            if currentCells[x][aboveCoord] == '#':
+                numNeighbors += 1 # Top neighbor is alive.
+            if currentCells[rightCoord][aboveCoord] == '#':
+                numNeighbors += 1 # Top-right neighbor is alive.
+            if currentCells[leftCoord][y] == '#':
+                numNeighbors += 1 # Left neighbor is alive.
+            if currentCells[rightCoord][y] == '#':
+                numNeighbors += 1 # Right neighbor is alive.
+            if currentCells[leftCoord][belowCoord] == '#':
+                numNeighbors += 1 # Bottom-left neighbor is alive.
+            if currentCells[x][belowCoord] == '#':
+                numNeighbors += 1 # Bottom neighbor is alive.
+            if currentCells[rightCoord][belowCoord] == '#':
+                numNeighbors += 1 # Bottom-right neighbor is alive.
+
+            # Set cell based on Conway's Game of Life rules:
+            if currentCells[x][y] == '#' and (numNeighbors == 2 or
+numNeighbors == 3):
+                # Living cells with 2 or 3 neighbors stay alive:
+                nextCells[x][y] = '#'
+            elif currentCells[x][y] == ' ' and numNeighbors == 3:
+                # Dead cells with 3 neighbors become alive:
+                nextCells[x][y] = '#'
+            else:
+                # Everything else dies or stays dead:
+                nextCells[x][y] = ' '
+    time.sleep(0.5) # Add pause to reduce flickering.
+*/
