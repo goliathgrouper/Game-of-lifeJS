7f474e52579cafa09ae2d6cd423392309545d53b
goliathgrouper <vitalijsimon48@gmail.com>
2024-09-04 19:06:34 +0300

    Clean up code, fix issue with grid-size edit

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ function getSquareSize(squareNumber) {
 }
 
 function generateGrid(n) {
+    nextGrid = [];
     let square;
     gridSize = n;
     let column;
@@ -31,7 +32,7 @@ function generateGrid(n) {
         nextGrid.push(column);
         
     }
-    //grid = [...container.querySelectorAll('#container > div')];
+  
 }
 
 generateGrid(16);
@@ -135,49 +136,3 @@ nextbtn.addEventListener('click', (event) => {
         }
     }
 });
-
-while (0) {
-    
-}
-/*# Calculate the next step's cells based on current step's cells:
-    for x in range(gridSize):
-        for y in range(HEIGHT):
-            # Get neighboring coordinates:
-            # `% WIDTH` ensures leftCoord is always between 0 and WIDTH - 1
-            leftCoord  = (x - 1) % WIDTH
-            rightCoord = (x + 1) % WIDTH
-            aboveCoord = (y - 1) % HEIGHT
-            belowCoord = (y + 1) % HEIGHT
-
-            # Count number of living neighbors:
-            numNeighbors = 0
-            if currentCells[leftCoord][aboveCoord] == '#':
-                numNeighbors += 1 # Top-left neighbor is alive.
-            if currentCells[x][aboveCoord] == '#':
-                numNeighbors += 1 # Top neighbor is alive.
-            if currentCells[rightCoord][aboveCoord] == '#':
-                numNeighbors += 1 # Top-right neighbor is alive.
-            if currentCells[leftCoord][y] == '#':
-                numNeighbors += 1 # Left neighbor is alive.
-            if currentCells[rightCoord][y] == '#':
-                numNeighbors += 1 # Right neighbor is alive.
-            if currentCells[leftCoord][belowCoord] == '#':
-                numNeighbors += 1 # Bottom-left neighbor is alive.
-            if currentCells[x][belowCoord] == '#':
-                numNeighbors += 1 # Bottom neighbor is alive.
-            if currentCells[rightCoord][belowCoord] == '#':
-                numNeighbors += 1 # Bottom-right neighbor is alive.
-
-            # Set cell based on Conway's Game of Life rules:
-            if currentCells[x][y] == '#' and (numNeighbors == 2 or
-numNeighbors == 3):
-                # Living cells with 2 or 3 neighbors stay alive:
-                nextCells[x][y] = '#'
-            elif currentCells[x][y] == ' ' and numNeighbors == 3:
-                # Dead cells with 3 neighbors become alive:
-                nextCells[x][y] = '#'
-            else:
-                # Everything else dies or stays dead:
-                nextCells[x][y] = ' '
-    time.sleep(0.5) # Add pause to reduce flickering.
-*/
